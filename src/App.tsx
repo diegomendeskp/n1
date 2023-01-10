1ebdc9326c9dd9fd2ab5344edc854b8871addc52
Hunter7p <diego.mendeskp@gmail.com>
2023-01-10 14:00:22 -0300

    footer and adjust

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import Footer from './components/organisms/Footer';
 import Header from './components/organisms/Header';
 import theme from './styles/index';
+import { Container } from './global/styled.t';
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
+      <Container>
+        <Header />
+        <Footer />
+      </Container>
     </ThemeProvider>
   );
 }
